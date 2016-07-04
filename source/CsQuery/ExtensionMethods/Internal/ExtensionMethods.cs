5a5ee40496f4f1fc177c926e4190082574f9dfea
Asbjørn Clemmensen <ac@siteimprove.com>
2016-07-04 21:25:23 +0200

    Getting rid of InvariantCulture.

--- source/CsQuery/ExtensionMethods/Internal/ExtensionMethods.cs
+++ source/CsQuery/ExtensionMethods/Internal/ExtensionMethods.cs
@@ -957,10 +957,6 @@ namespace CsQuery.ExtensionMethods.Internal
                     return StringComparer.CurrentCulture;
                 case StringComparison.CurrentCultureIgnoreCase:
                     return StringComparer.CurrentCultureIgnoreCase;
-                case StringComparison.InvariantCulture:
-                    return StringComparer.InvariantCulture;
-                case StringComparison.InvariantCultureIgnoreCase:
-                    return StringComparer.InvariantCultureIgnoreCase;
                 case StringComparison.Ordinal:
                     return StringComparer.Ordinal;
                 case StringComparison.OrdinalIgnoreCase:
