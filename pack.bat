71a3de9c9da58c4b2e3c61450c4b4c4929e8942e
Asbjørn Clemmensen <ac@siteimprove.com>
2017-09-19 17:15:29 +0200

    Always build nuget packages in release mode

--- pack.bat
+++ pack.bat
@@ -1,2 +1,2 @@
-dotnet pack -o %~dp0\pack source/CsQuery/CsQuery.csproj
-dotnet pack -o %~dp0\pack source/HtmlParserSharp/HtmlParserSharp.csproj
+dotnet pack -c Release -o %~dp0\pack source/CsQuery/CsQuery.csproj
+dotnet pack -c Release -o %~dp0\pack source/HtmlParserSharp/HtmlParserSharp.csproj
